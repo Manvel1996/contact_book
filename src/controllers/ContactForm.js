77a9ee4b9b4072d983e4498062bd878c71d28a8b
Manvel1996 <mdanielyan1996@gmail.com>
2023-07-10 10:51:55 +0400

    add trim in input form

--- src/controllers/ContactForm.js
+++ src/controllers/ContactForm.js
@@ -1,37 +1,37 @@
 export function userNameControl(e, setUserName, setUserNameErr) {
-  setUserName(e.target.value);
+  setUserName(e.target.value.trim());
 
-  if (e.target.value.length >= 2 && e.target.value.length <= 20) {
+  if (e.target.value.trim().length >= 2 && e.target.value.trim().length <= 20) {
     setUserNameErr(false);
   } else setUserNameErr(true);
 }
 
 export function surnameControl(e, setSurname, setSurnameErr) {
-  setSurname(e.target.value);
+  setSurname(e.target.value.trim());
 
-  if (e.target.value.length >= 2 && e.target.value.length <= 20) {
+  if (e.target.value.trim().length >= 2 && e.target.value.trim().length <= 20) {
     setSurnameErr(false);
   } else setSurnameErr(true);
 }
 
 export function emailControl(e, setEmail, setEmailErr) {
-  setEmail(e.target.value);
+  setEmail(e.target.value.trim());
 
   if (
-    e.target.value.length === 0 ||
-    (/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(e.target.value) &&
-      e.target.value.length < 30)
+    e.target.value.trim().length === 0 ||
+    (/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[A-Za-z]+$/.test(e.target.value.trim()) &&
+      e.target.value.trim().length < 30)
   ) {
     setEmailErr(false);
   } else setEmailErr(true);
 }
 
 export function phoneControl(e, setPhone, setPhoneErr) {
-  setPhone(e.target.value);
+  setPhone(e.target.value.trimLeft());
 
   if (
     /^[\+]?[(]?[0-9]{3}[)]?[\s]?[0-9]{2}[\s]?[0-9]{3}[\s]?[0-9]{3}$/.test(
-      e.target.value
+      e.target.value.trimLeft()
     )
   ) {
     setPhoneErr(false);
@@ -39,12 +39,12 @@ export function phoneControl(e, setPhone, setPhoneErr) {
 }
 
 export function photoUrlControl(e, setPhotoUrl, setPhotoUrlErr) {
-  setPhotoUrl(e.target.value);
+  setPhotoUrl(e.target.value.trim());
 
   if (
-    e.target.value.length === 0 ||
+    e.target.value.trim().length === 0 ||
     /^(http(s):\/\/.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/.test(
-      e.target.value
+      e.target.value.trim()
     )
   ) {
     setPhotoUrlErr(false);
