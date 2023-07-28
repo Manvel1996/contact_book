7817f3eb23f710e532a65a2c5b17d781b4e333d1
Manvel1996 <mdanielyan1996@gmail.com>
2023-07-28 12:32:13 +0400

    change new crud link

--- src/constants/Api.js
+++ src/constants/Api.js
@@ -1,4 +1,4 @@
 export const BASE_URL =
-  "https://crudcrud.com/api/9ceb1140729f46bb90fd874c20454946";
+  "https://crudcrud.com/api/e25cab7b6a004db790562999a4e17cf0";
 
 export const CURRENT_API = "auth";
